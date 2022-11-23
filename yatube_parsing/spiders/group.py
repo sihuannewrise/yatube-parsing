7bbd0f275f5bf311787733fef800f83e61491ccd
sihuannewrise <sihuannewrise@gmail.com>
2022-11-23 16:08:44 +0300

    group spider

---
+++ yatube_parsing/spiders/group.py
@@ -0,0 +1,28 @@
+import scrapy
+
+
+class GroupSpider(scrapy.Spider):
+    name = 'group'
+    allowed_domains = ['51.250.32.185']
+    start_urls = ['http://51.250.32.185/']
+
+    def parse(self, response):
+        all_groups = response.css('a[href^="/group/"]')
+        for group_link in all_groups:
+            return response.follow(group_link, callback=self.parse_group)
+
+        next_page = response.xpath("//a[contains(., 'Следующая')]/@href").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+    def parse_group(self, response):
+        group = response.css('div.card')
+        yield {
+            'group_name': group.css('h2::text').strip(),
+            'description': group.css('p.group_descr::text').strip(),
+            'posts_count': int(
+                group.css(
+                        'div.posts_count::text'
+                    ).replace('Записей: ', '').strip()
+            ),
+        }
--- yatube_parsing/spiders/yatube.py
+++ yatube_parsing/spiders/yatube.py
@@ -7,4 +7,15 @@ class YatubeSpider(scrapy.Spider):
     start_urls = ['http://51.250.32.185/']
 
     def parse(self, response):
-        pass
+        for post in response.css('div.card-body'):
+            yield {
+                'author': post.css('strong.d-block::text').get(),
+                'text': ' '.join(
+                    t.strip() for t in post.css('p.card-text::text').getall()
+                ),
+                'date': post.css('small.text-muted::text').get(),
+            }
+
+        next_page = response.xpath("//a[contains(., 'Следующая')]/@href").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
