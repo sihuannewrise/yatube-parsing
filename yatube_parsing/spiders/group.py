b4f19b741a1bb6bca55c805d0be0d5209f106f78
sihuannewrise <sihuannewrise@gmail.com>
2022-11-23 16:52:10 +0300

    group

--- yatube_parsing/spiders/group.py
+++ yatube_parsing/spiders/group.py
@@ -7,7 +7,7 @@ class GroupSpider(scrapy.Spider):
     start_urls = ['http://51.250.32.185/']
 
     def parse(self, response):
-        all_groups = response.css('a[href^="/group/"]')
+        all_groups = response.css('a.group_link::attr(href)')
         for group_link in all_groups:
             return response.follow(group_link, callback=self.parse_group)
 
@@ -19,10 +19,9 @@ class GroupSpider(scrapy.Spider):
         group = response.css('div.card')
         yield {
             'group_name': group.css('h2::text').strip(),
-            'description': group.css('p.group_descr::text').strip(),
+            'description': group.css('p.group_descr::text'),
             'posts_count': int(
-                group.css(
-                        'div.posts_count::text'
-                    ).replace('Записей: ', '').strip()
+                ' '.join(t.strip() for t in group.css(
+                    'div.posts_count::text').getall()).split()[1]
             ),
         }
